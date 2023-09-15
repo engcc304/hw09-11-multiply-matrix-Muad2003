64490114555fdc9f08d2d8c4e34944c98d873473
Muad2003 <anuphap2003118@gmail.com>
2023-09-15 23:11:33 +0700

    updatehowork 9-11

--- Homework/HW09/hw09-11-Multiply-Matrix.cpp
+++ Homework/HW09/hw09-11-Multiply-Matrix.cpp
@@ -30,3 +30,81 @@
             30 36
 
 */
+
+#include <stdio.h>
+
+int main () {
+    
+    int column1 , row1 ;
+    int column2 , row2 ;
+    // สร้างตัวแปร array1
+    printf ( "Array1 Value :\n" ) ;
+    scanf ( "%d %d", &row1 , &column1 ) ;
+    int array1[ row1 ] [ column1 ] ;
+    for ( int i = 0 ; i < row1 ; i++ ) {
+        for ( int k = 0 ; k < column1 ; k++ ) {
+            scanf ( "%d", &array1[ i ] [ k ] ) ;
+        } // end for
+    } // end for
+    
+    // สร้างตัวแปร array2
+    printf ( "Array2 Value :\n" ) ;
+    scanf ( "%d %d", &row2 , &column2  ) ;
+    int array2[ row2 ] [ column2 ] ;
+    for ( int i = 0 ; i < row2 ; i++ ) {
+        for ( int k = 0 ; k < column2 ; k++ ) {
+            scanf ( "%d", &array2[ i ] [ k ] ) ;
+        } // end for
+    } // end for
+    
+    // แสดง array1
+    printf ( "Array1\n" ) ;
+    for ( int i = 0 ; i < row1 ; i++ ) {
+        for ( int k = 0 ; k < column1 ; k++ ) {
+            printf ( "%d", array1[ i ] [ k ] ) ;
+            if ( column1 > k + 1 ) {
+                printf ( " " ) ;
+            } // end if
+        } // end for
+        printf ( "\n" ) ;
+    } // end for
+
+    // แสดง array2
+    printf ( "Array2\n" ) ;
+    for ( int i = 0 ; i < row2 ; i++ ) {
+        for ( int k = 0 ; k < column2 ; k++ ) {
+            printf ( "%d", array2[ i ] [ k ] ) ;
+            if ( column2 > k + 1 ) {
+                printf ( " " ) ;
+            } // end if
+        } // end for
+        printf ( "\n" ) ;
+    } // end for
+    
+    // บวกเมทริ
+    int matrix[ row1 ] [ column2 ] = { } ;
+    if ( column1 == row2 ) {
+        
+        for ( int i = 0 ; i < row1 ; i++ ) {
+            for ( int k = 0 ; k < column2 ; k++ ) {
+                for ( int j = 0 ; j < column1 ; j++ ) {
+                    matrix[ i ] [ k ] += array1[ i ] [ j ] * array2[ j ] [ k ] ;
+                } // end for
+            } // end for
+        } // end for
+    } // end if
+    
+    // แสดงค่า
+    printf ( "Array1 x Array2\n" ) ;
+    for ( int i = 0 ; i < row1 ; i++ ) {
+        for ( int k = 0 ; k < column2 ; k++ ) {
+            printf ( "%d", matrix[ i ] [ k ]  ) ;
+            if ( k + 1 < column2 ) {
+                printf ( " " ) ;
+            } // end if
+        } // end for
+        printf ( "\n" ) ;
+    } // end for
+
+    return 0 ;
+} // end for
